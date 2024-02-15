dfe7a07914256d78cdc521e2e4c140d9cb2458eb
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-02-15 08:29:01 +0530

    Time: 126 ms (97.03%) | Memory: 83.4 MB (34.58%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     long long largestPerimeter(vector<int>& nums) {
         long long ans = 0;
+        long long prev = 0;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        vector<long long> prefix(n);
-        prefix[0] = nums[0];
-        prefix[1] = nums[0];
-        for(int i = 2;i<n;i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-        }
+        // vector<long long> prefix(n);
+        // prefix[0] = nums[0];
+        // prefix[1] = nums[0];
+        // for(int i = 2;i<n;i++){
+        //     prefix[i] = prefix[i-1] + nums[i-1];
+        // }
         // for(auto x:prefix)    cout<<x<<" ";
         // cout<<endl;
+        
+        prev = nums[0] + nums[1];
         for(int i = 2;i<n;i++){
-            if(prefix[i]>nums[i]){
-                ans = max(ans, prefix[i] + nums[i]);
+            if(prev>nums[i]){
+                ans = max(ans, prev + nums[i]);
             }
             // cout<<ans<<" ";
+            prev = prev + nums[i];
         }
         return ans==0?-1:ans;
     }
