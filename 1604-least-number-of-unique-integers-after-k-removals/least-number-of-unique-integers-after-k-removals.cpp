a0ef8673e11033de9aeeb7d3462c76a5dcfdc763
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-02-16 11:12:27 +0530

    Time: 120 ms (78.33%) | Memory: 66.1 MB (51.33%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -5,17 +5,34 @@ public:
         unordered_map<int,int> mp;
         for(auto x:arr) mp[x]++;
         
-        vector<int> freq_count;
-        for(auto elem:mp)   freq_count.push_back(elem.second);
-        sort(freq_count.begin(),freq_count.end());
-        int i = 0;
-        for(;i<freq_count.size();i++){
-            if(k>=freq_count[i]){
-                k -= freq_count[i];
+        // vector<int> freq_count;
+        // for(auto elem:mp)   freq_count.push_back(elem.second);
+        // sort(freq_count.begin(),freq_count.end());      // sorting requires nlogn
+        // int i = 0;
+        // for(;i<freq_count.size();i++){
+        //     if(k>=freq_count[i]){
+        //         k -= freq_count[i];
+        //     }
+        //     else break;
+        // }
+        
+        
+        
+        // min-heap nlogk
+        
+        priority_queue<int,vector<int>, greater<int>> pq;
+        for(auto x:mp)  pq.push({x.second});
+        
+        while(k>0 and !pq.empty()){
+            int minfreq = pq.top();
+            if(k>=minfreq)
+            {
+                k-=minfreq;
+                pq.pop();
             }
             else break;
         }
         
-        return freq_count.size()-i;
+        return pq.size();
     }
 };
