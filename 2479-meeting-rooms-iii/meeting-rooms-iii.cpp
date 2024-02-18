fdaca3de6de2154197c261ef8bfcc238423868c5
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-02-18 18:06:43 +0530

    Time: 330 ms (77.80%) | Memory: 113.7 MB (30.48%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,38 +1,88 @@
 class Solution {
 public:
+    typedef pair<long long,int> P;
+    
     int mostBooked(int n, vector<vector<int>>& meetings) {
         
         sort(begin(meetings),end(meetings));
         
-        vector<long long> lastEndingTime(n,0);
+//         vector<long long> lastEndingTime(n,0);
         vector<int> roomUsedCount(n,0);
         
-        for(auto meeting:meetings){
-            int start = meeting[0];
-            int end   = meeting[1];
+//         for(auto meeting:meetings){
+//             int start = meeting[0];
+//             int end   = meeting[1];
+//             int duration = end - start;
+            
+//             int earlyEndRoom = 0;
+//             long long earlyEndTime = LLONG_MAX;
+//             bool isRoomFree = false;
+//             for(int room = 0;room<n;room++){
+//                 if(lastEndingTime[room]<=start){        // got free room
+//                     roomUsedCount[room]++;
+//                     lastEndingTime[room] = end;
+//                     isRoomFree = true;
+//                     break;
+//                 }
+//                 // room nhi mila
+                
+//                 if(earlyEndTime>lastEndingTime[room]){
+//                     earlyEndTime = lastEndingTime[room];
+//                     earlyEndRoom = room;
+//                 }
+//             }
+            
+//             if(!isRoomFree){
+//                 roomUsedCount[earlyEndRoom]++;
+//                 lastEndingTime[earlyEndRoom] += duration;
+//             }
+//         }
+        
+//         int resultRoom = -1;
+//         int freq = 0;
+        
+//         for(int room = 0;room<n;room++){
+//             if(roomUsedCount[room]>freq){
+//                 resultRoom = room;
+//                 freq = roomUsedCount[room];
+//             }
+//         }
+        
+//         return resultRoom;
+        
+        priority_queue<P,vector<P>,greater<P>> usedRooms;
+        
+        priority_queue<int,vector<int>,greater<int>> availableRooms;
+        
+        // initially all rooms are free
+        for(int room = 0;room<n;room++) availableRooms.push(room);
+        
+        for(auto meet:meetings){
+            int start = meet[0];
+            int end = meet[1];
             int duration = end - start;
             
-            int earlyEndRoom = 0;
-            long long earlyEndTime = LLONG_MAX;
-            bool isRoomFree = false;
-            for(int room = 0;room<n;room++){
-                if(lastEndingTime[room]<=start){        // got free room
-                    roomUsedCount[room]++;
-                    lastEndingTime[room] = end;
-                    isRoomFree = true;
-                    break;
-                }
-                // room nhi mila
+            while(!usedRooms.empty() && usedRooms.top().first<=start){
+                int room = usedRooms.top().second;
+                usedRooms.pop();
                 
-                if(earlyEndTime>lastEndingTime[room]){
-                    earlyEndTime = lastEndingTime[room];
-                    earlyEndRoom = room;
-                }
+                availableRooms.push(room);
             }
-            
-            if(!isRoomFree){
-                roomUsedCount[earlyEndRoom]++;
-                lastEndingTime[earlyEndRoom] += duration;
+            if(!availableRooms.empty()){
+                int room = availableRooms.top();
+                availableRooms.pop();
+                roomUsedCount[room]++;
+                usedRooms.push({end,room});
+            }
+            else{
+                //  no room available pick earliest
+                int room = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+                usedRooms.pop();
+                
+                usedRooms.push({endTime+duration,room});
+                roomUsedCount[room]++;
+                
             }
         }
         
@@ -47,5 +97,6 @@ public:
         }
         
         return resultRoom;
+        
     }
 };
