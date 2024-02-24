3a1e8ee8f4094a10cb95118a5a8a74f18f92a3ea
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-02-24 15:52:10 +0530

    Time: 451 ms (74.18%) | Memory: 179.3 MB (53.69%) - LeetSync

---
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        vector<pair<int,int>> adj[n];
+        
+        //constructing graph
+        
+        for(auto it:meetings){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        
+        // store key
+        vector<bool> hasKey(n,false);
+        
+        // priority queue on the basis of time
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+        q.push({0,0});
+        q.push({0,firstPerson});
+        
+        while(!q.empty()){
+            auto it = q.top();
+            q.pop();
+            if(hasKey[it.second])   continue;
+            hasKey[it.second] = true;
+            for(auto node:adj[it.second]){
+                int p = node.first;
+                int tm = node.second;
+                
+                if(tm>=it.first and !hasKey[p]){
+                    q.push({tm,p});
+                }
+            }
+        }
+        
+        vector<int> ans;
+        for(int i = 0;i<n;i++){
+            if(hasKey[i])   ans.push_back(i);
+        }
+        return ans;
+    }
+};
