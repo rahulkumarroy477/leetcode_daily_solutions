ca85c61e6b77f30ee5a7025e820f96e6fd9b3004
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-03-03 11:57:25 +0530

    Time: 6 ms (29.56%) | Memory: 13.4 MB (53.77%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,22 +4,44 @@ public:
         // hashing
         
         int n = nums.size();
-        map<int,int> mp;
-        for(const auto &x:nums){
-            mp[x]++;
-        }
-        int i = 0;
-        for(const auto &x:mp){
-            int a = x.first;
-            int freq = x.second;
-            if(freq==1){
-                nums[i++] = a;
-            }else{
-                nums[i] = nums[i+1] = a;
-                i += 2;
+        // map<int,int> mp;
+        // for(const auto &x:nums){
+        //     mp[x]++;
+        // }
+        // int i = 0;
+        // for(const auto &x:mp){
+        //     int a = x.first;
+        //     int freq = x.second;
+        //     if(freq==1){
+        //         nums[i++] = a;
+        //     }else{
+        //         nums[i] = nums[i+1] = a;
+        //         i += 2;
+        //     }
+        // }
+        // return i;
+        
+        // two pointer
+        
+        if (n <= 2)
+        return n;
+
+    int i = 0;
+
+    for (int j = 1, count = 1; j < n; ++j) {
+        if (nums[i] == nums[j]) {
+            if (count < 2) {
+                ++i;
+                nums[i] = nums[j];
+                ++count;
             }
+        } else {
+            ++i;
+            nums[i] = nums[j];
+            count = 1;
         }
-        return i;
+    }
+    return i + 1;
         
     }
 };
