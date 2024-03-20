e18d04078fb8adf2f9fc431f5776e131f0749372
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-03-20 19:08:27 +0530

    Time: 179 ms (58.43%) | Memory: 98.1 MB (44.80%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -11,18 +11,33 @@
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode *temp1=list1,*temp2=NULL,*del=NULL;
-        for(int i=1;i<a;i++) temp1=temp1->next;
-        del = temp1->next;
-        temp1->next=list2;
-        for(int i = a;i<=b;i++) 
-        {
-            temp2=del->next;
-            delete(del);
-            del=temp2;
-        }
-        while(list2->next!=NULL) list2=list2->next;
-        list2->next=temp2;
+        // ListNode *temp1=list1,*temp2=NULL,*del=NULL;
+        // for(int i=1;i<a;i++) temp1=temp1->next;
+        // del = temp1->next;
+        // temp1->next=list2;
+        // for(int i = a;i<=b;i++) 
+        // {
+        //     temp2=del->next;
+        //     delete(del);
+        //     del=temp2;
+        // }
+        // while(list2->next!=NULL) list2=list2->next;
+        // list2->next=temp2;
+        // return list1;
+        
+        ListNode* ptr = list1;
+        for (int i = 0; i < a - 1; ++i)
+            ptr = ptr->next;
+        
+        ListNode* qtr = ptr->next;
+        for (int i = 0; i < b - a + 1; ++i)
+            qtr = qtr->next;
+        
+        ptr->next = list2;
+        while (list2->next)
+            list2 = list2->next;
+        list2->next = qtr;
+        
         return list1;
         
     }
