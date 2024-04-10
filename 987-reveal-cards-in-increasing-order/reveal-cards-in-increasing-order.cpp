69dceef885d710057d1e0f4b7ce9e126a91edb13
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-04-10 23:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (10.89%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -4,20 +4,37 @@ public:
         sort(deck.begin(),deck.end());
         int n = deck.size();
         vector<int> ans(n,0);
-        int i = 0, j = 0;
-        bool skip = false;
-        while(i<n){
-            if(ans[j] == 0)
-            {
-                if(!skip){
-                    ans[j] = deck[i];
-                    i++;
-                }
+        
+        // Approach 1
+//         int i = 0, j = 0;
+//         bool skip = false;
+//         while(i<n){
+//             if(ans[j] == 0)
+//             {
+//                 if(!skip){
+//                     ans[j] = deck[i];
+//                     i++;
+//                 }
                 
-                skip = !skip;
+//                 skip = !skip;
+//             }
+//             j = (j+1)%n;
+//         }
+        
+        // Approach 2 using queue
+        queue<int> q;
+        for(int i = 0;i<n;i++)  q.push(i);
+        int j = 0;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            if(!q.empty()){
+                q.push(q.front());
+                q.pop();
             }
-            j = (j+1)%n;
+            ans[front] = deck[j++];
         }
+        
         
         return ans;
     }
