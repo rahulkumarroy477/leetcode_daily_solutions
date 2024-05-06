cde5acec243b36cc0a4a8b5a7fb6e93b0de1e4ef
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-05-06 20:27:57 +0530

    Time: 343 ms (6.23%) | Memory: 170.6 MB (29.59%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -8,36 +8,57 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
+To reverse the list
+Approach1: Stack-->creating new Linked list
+Approach2: Recursion
+Approach3: Reverse the list
+*/
 class Solution {
 public:
+    
     ListNode* removeNodes(ListNode* head) {
         // right to left traversal
-        stack<ListNode*> st;
-        ListNode* curr = head;
+//         stack<ListNode*> st;
+//         ListNode* curr = head;
         
-        while(curr!=NULL){
-            st.push(curr);
-            curr = curr->next;
+//         while(curr!=NULL){
+//             st.push(curr);
+//             curr = curr->next;
+//         }
+        
+//         curr = st.top();
+//         st.pop();
+        
+//         int maxVal = curr->val;
+//         ListNode* newHead = new ListNode(curr->val);
+//         while(!st.empty()){
+//             curr = st.top();
+//             st.pop();
+//             if(curr->val<maxVal){
+//                 continue;
+//             }
+            
+//             ListNode* newNode = new ListNode(curr->val);
+//             newNode->next = newHead;
+//             newHead = newNode;
+//             maxVal = curr->val;
+            
+//         }
+//         return newHead;
+        
+        // Approach 2
+        if(head==NULL || head->next == NULL)    return head;
+        
+        ListNode* newNode = removeNodes(head->next);
+        
+        if(head->val < newNode->val){
+            delete head;
+            return newNode;
         }
         
-        curr = st.top();
-        st.pop();
-        
-        int maxVal = curr->val;
-        ListNode* newHead = new ListNode(curr->val);
-        while(!st.empty()){
-            curr = st.top();
-            st.pop();
-            if(curr->val<maxVal){
-                continue;
-            }
-            
-            ListNode* newNode = new ListNode(curr->val);
-            newNode->next = newHead;
-            newHead = newNode;
-            maxVal = curr->val;
-            
-        }
-        return newHead;
+        head->next = newNode;
+        return head;
     }
 };
