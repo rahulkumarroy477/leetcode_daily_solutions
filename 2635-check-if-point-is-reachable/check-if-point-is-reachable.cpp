58f487e1e1d432e55bb06aaadfdbb8418cc4b9e7
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-06-25 15:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (50.00%) - LeetSync

--- 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
+++ 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     bool isReachable(int x, int y) {
-        int gc = __gcd(x,y);
-        return (gc&(gc-1))==0;    // even check
+        // int gc = __gcd(x,y);
+        // return (gc&(gc-1))==0;    // even check
+
+        int i = x, j = y;
+        while(i>0 and j>0){
+            while(i%2==0)   i/=2;
+            while(j%2==0)   j/=2;
+
+            if(i>j) i-=j;
+            else if(j>i)    j-=i;
+            else{
+                if(i==1 && j==1)    return 1;
+                else i-=j;  // yaha either i-= j or j-= i
+            }
+        }
+        return 0;
     }
 };
