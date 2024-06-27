b15aaccd1564ff6fff4fe1df4c5e406157ee195a
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-06-27 11:57:22 +0530

    Time: 3 ms (37.84%) | Memory: 8.2 MB (66.30%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -4,7 +4,9 @@ public:
        {'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},
        {'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}
     };
-    void solve(string &temp,int i,string digits,vector<string> &ans){
+    vector<string> ans;
+    string temp;
+    void solve(int i,string digits){
         if(i==digits.length()){
             ans.push_back(temp);
             return;
@@ -12,7 +14,7 @@ public:
 
         for(char ch: mp[digits[i]]){
             temp.push_back(ch);
-            solve(temp,i+1,digits,ans);
+            solve(i+1,digits);
             temp.pop_back();
         }
 
@@ -20,9 +22,7 @@ public:
     }
     vector<string> letterCombinations(string digits) {
         if(digits.length()==0)  return {};
-        vector<string> ans;
-        string temp;
-        solve(temp,0,digits,ans);
+        solve(0,digits);
         return ans;
     }
 };
