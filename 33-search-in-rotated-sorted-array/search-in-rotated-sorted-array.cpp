2cdd29cbb7aefeb7590d00691010b14f1f45cdf1
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-06-28 15:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (77.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // simple search
         int low = 0;
-        int high = nums.size() - 1;
-        
+        int high = nums.size()-1;
+
         while(low<=high){
-            
             int mid = low + (high-low)/2;
             if(nums[mid] == target) return mid;
-            
-            // left part is sorted
-            if(nums[low]<=nums[mid])
-            {
-                if(nums[low]<=target and target<=nums[mid]) 
+            if(nums[mid]>=nums[low]){
+                if(target>=nums[low] && target<=nums[mid])
                     high = mid - 1;
                 else
-                    low = mid + 1;
+                    low = mid + 1; 
             }
             else{
-                if(nums[mid]<=target and target<=nums[high])
+                if(target>=nums[mid] && target<=nums[high])
                     low = mid + 1;
                 else
                     high = mid - 1;
-            }
+            }  
         }
         return -1;
     }
