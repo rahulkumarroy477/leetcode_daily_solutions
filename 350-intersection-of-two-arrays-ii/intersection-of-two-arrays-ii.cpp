0486e265ec7d3aa1c425105af52339bcb09a269c
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-07-02 16:05:14 +0530

    Time: 4 ms (59.42%) | Memory: 13.6 MB (30.53%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int i = 0, j = 0;
-        vector<int> ans;
+        // O(nlogn)
+        // sort(nums1.begin(),nums1.end());
+        // sort(nums2.begin(),nums2.end());
+        // int n1 = nums1.size();
+        // int n2 = nums2.size();
+        // int i = 0, j = 0;
+        // vector<int> ans;
 
-        while(i<n1 && j<n2){
-            if(nums1[i]==nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
+        // while(i<n1 && j<n2){
+        //     if(nums1[i]==nums2[j]){
+        //         ans.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j]){
+        //         i++;
+        //     }
+        //     else j++;
+        // }
+
+        // O(n1+n2)
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(const int& x:nums1){
+            mp[x]++;
+        }
+
+        for(const int &x:nums2){
+            if(mp.find(x)!=mp.end()){
+                ans.push_back(x);
+                mp[x]--;
+                if(mp[x]==0){
+                    mp.erase(x);
+                }
             }
-            else if(nums1[i]<nums2[j]){
-                i++;
-            }
-            else j++;
         }
 
         return ans;
