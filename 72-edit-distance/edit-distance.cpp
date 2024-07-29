ef9b079177074499eccd6d48001cf6f470311d9e
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-07-29 23:44:02 +0530

    Time: 27 ms (21.46%) | Memory: 13.5 MB (19.44%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,14 +4,14 @@ int t[501][501];
 int solve(int i, int j, string s1, string s2){
     // s1 becomes empty then s1="" and s2 = "abc" to convert s1 to s2 we will perform
     // 3 insert operations i.e.., return j + 1
-    if(i<0) return j + 1;
+    if(i==0) return j;
 
     // if s2 becomes empty s2="" and s1="abc"   need 3 delete operations
-    if(j<0) return i + 1;
+    if(j==0) return i;
 
     if(t[i][j]!=-1) return t[i][j];
-    
-    if(s1[i]==s2[j]){
+
+    if(s1[i-1]==s2[j-1]){
         return t[i][j] =  solve(i-1,j-1,s1,s2);
     }
 
@@ -27,7 +27,27 @@ public:
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
-        memset(t,-1,sizeof(t));
-        return solve(m-1,n-1,word1,word2);
+        // memset(t,-1,sizeof(t));
+        // return solve(m,n,word1,word2);
+
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i = 0;i<=m;i++){
+            dp[i][0] = i;
+        }
+        for(int i = 0;i<=n;i++){
+            dp[0][i] = i;
+        }
+        for(int i = 1;i<=m;i++){
+            for(int j = 1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
