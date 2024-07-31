075ea2f635c54e8fb52fdd6637dec887dfc696bf
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-07-31 23:58:05 +0530

    Time: 81 ms (72.88%) | Memory: 79.4 MB (29.90%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,37 +1,46 @@
 class Solution {
-vector<vector<string>>ans;
-bool isPalindrome(string s){
-    int n=s.size();
-    int i=0;
-    int j=n-1;
-    while(i<=j){
-        if(s[i]!=s[j])
-            return false;
-            i++;
-            j--;
+bool palindrome[17][17];
+vector<vector<string>> ans;
+vector<string> temp;
+void setPalindrome(string &s){
+
+    int n = s.length();
+    // one length
+    for(int i = 0;i<n;i++){
+        palindrome[i][i] = true;
     }
-    return true;
+    // two length
+    for(int i = 0;i<n-1;i++){
+        if(s[i]==s[i+1])
+            palindrome[i][i+1] = true;
+    }
+
+    for(int length = 3;length<=n;length++){
+        for(int i = 0;i<=n-length;i++){
+            int j = i + length - 1;
+            if(s[i]==s[j] && palindrome[i+1][j-1])
+                palindrome[i][j] = true;
+        }
+    }
 }
-void solve(int i,int n,string &s,vector<string>helper){
-    if(i==n){
-        ans.push_back(helper);
+
+void solve(int i, string &s){
+    if(i == s.length()){
+        ans.push_back(temp);
         return;
     }
-    string str="";
-    for(int k=i;k<n;k++){
-        str.push_back(s[k]);
-        if(isPalindrome(str)){
-            helper.push_back(str);
-            solve(k+1,n,s,helper);
-            helper.pop_back();
+    for(int j = i;j<s.length();j++){
+        if(palindrome[i][j]){
+            temp.push_back(s.substr(i,j-i+1));
+            solve(j+1,s);
+            temp.pop_back();
         }
     }
 }
 public:
     vector<vector<string>> partition(string s) {
-        int n=s.size();
-        vector<string>helper;
-        solve(0,n,s,helper);
+        setPalindrome(s);
+        solve(0,s);
         return ans;
     }
 };
