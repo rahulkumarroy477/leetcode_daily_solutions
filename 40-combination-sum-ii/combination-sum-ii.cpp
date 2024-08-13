811a9224cdeff1514af551564fd94a85c9775283
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-08-13 15:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (34.60%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,39 +1,26 @@
 class Solution {
-public:
-    
-    void solve(int i,int target,vector<int>& candidates,vector<vector<int>>& ans,vector<int> &temp)
-    {
-        // if(i==candidates.size()){
-        //     if(target==0)   ans.push_back(temp);
-        //     return;
-        // }
-        
-        if(target==0){
+    vector<vector<int>> ans;
+    vector<int> temp;
+    int n;
+    void solve(int idx,int target, vector<int>& nums){
+        if(target == 0){
             ans.push_back(temp);
             return;
         }
-        
-        // include
-        int prev = -1;
-        
-        for(int k = i;k<candidates.size();k++){
-            if(k>i and candidates[k] == candidates[k-1]){   // atleast include first occurence k>i
-                continue;
-            }
-            if(candidates[k]>target)    break;  // abhi bada ho gaya to baad wala bhi bada rhega
-            temp.push_back(candidates[k]);
-            solve(k+1,target-candidates[k],candidates,ans,temp);
+
+        for(int i = idx;i<n;i++){
+            if(i>idx && nums[i]==nums[i-1]) continue;   // skip duplicates
+            if(nums[i]>target)  break;  // no need to explore
+            temp.push_back(nums[i]);
+            solve(i+1,target-nums[i],nums);
             temp.pop_back();
         }
-        // // exclude
-        // solve(i+1,target,candidates,ans,temp);
     }
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        sort(candidates.begin(),candidates.end());
-        vector<int> temp;
-        solve(0,target,candidates,ans,temp);
-        
+        n = candidates.size();
+        sort(begin(candidates),end(candidates));
+        solve(0,target,candidates);
         return ans;
     }
 };
