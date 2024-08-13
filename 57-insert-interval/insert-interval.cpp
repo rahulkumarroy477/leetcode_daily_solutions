c521c2157fb5284c7c6250a050602a1b5b32742a
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-08-13 16:24:47 +0530

    Time: 4 ms (96.21%) | Memory: 20.7 MB (58.16%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,29 +2,35 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
-        if(n==0)    return {newInterval};
-        vector<vector<int>> ans;
         
-        // add all elements that end before interval
+        int newSt = newInterval[0];
+        int newEnd = newInterval[1];
         int i = 0;
-        while(i<n && intervals[i][1]<newInterval[0]){
+        vector<vector<int>> ans;
+
+        // find position where it lies
+        while(i<n && intervals[i][1]<newSt){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        // merge the intervals
+
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(intervals[i][0],
+            newInterval[0]);
+            newInterval[1] = max(intervals[i][1],
+            newInterval[1]);
+            i++;
+        }
+
+        ans.push_back(newInterval);
+
+        while(i<n){
             ans.push_back(intervals[i]);
             i++;
         }
         
-        // merging from newInterval
-        while(i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0] = min(newInterval[0],intervals[i][0]);
-            newInterval[1] = max(newInterval[1],intervals[i][1]);
-            i++;
-        }
-        ans.push_back(newInterval);
-        
-        // remaining elements
-        while(i<n){
-            ans.push_back(intervals[i++]);
-        }
-        
+
         return ans;
     }
 };
