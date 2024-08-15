6e4c92664a007908bbb883cd74d1c84166af07ce
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-08-15 17:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.23%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,15 +6,11 @@ public:
         long long ans = INT_MIN;
         long long pre = 1, suff = 1;
         for(int i = 0;i<n;i++){
-            if(pre == 0)    pre = 1;
-            if(suff == 0)   suff = 1;
+            if(pre == 0 || abs(pre) > INT_MAX)    pre = 1;
+            if(suff == 0 || abs(suff) > INT_MAX)   suff = 1;
             pre = pre * nums[i];
             suff = suff * nums[n-i-1] * 1LL;
             ans = max(ans,max(pre,suff));
-
-            // Prevent overflow by resetting pre and suff to prevent accumulation
-        if (abs(pre) > INT_MAX) pre = nums[i];  // Reset pre if it exceeds int limits
-        if (abs(suff) > INT_MAX) suff = nums[n - i - 1];  // Reset suff if it exceeds int limits
         }
 
 
