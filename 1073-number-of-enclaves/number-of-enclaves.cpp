a5371aefb940cc68ba4ced892662fcfe5fc6f87d
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-09-05 15:04:59 +0530

    Time: 1086 ms (5.02%) | Memory: 43.1 MB (16.55%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,52 +1,59 @@
 class Solution {
+
+private:
+    int m, n;
+    vector<vector<bool>> vis;
+
+    bool ok(int x, int y){
+        return (x>=0 && x<m && y>=0 && y<n && !vis[x][y]);
+    }
+    
+    //directions
+    vector<pair<int,int>> dir{{0,-1},{-1,0},{0,1},{1,0}};
+    
+
+    // dfs with size
+    int dfs(int x, int y, vector<vector<int>> &grid){
+        vis[x][y] = true;
+        int count = 1;
+
+        for(auto [dx,dy]:dir){
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if(ok(nx,ny) && grid[nx][ny]==1){
+                count += dfs(nx,ny,grid);
+            }
+        }
+
+        return count;
+    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        
-        // mutlisource bfs
-        queue<pair<int,int>> q;
-        // insert all boundary 1's
-        
+        m = grid.size();
+        n = grid[0].size();
+
+        vis.resize(m,vector<bool>(n,false));
+
+        // boundary marking
         for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if((i==0 or i==m-1 or j==0 or j==n-1) and grid[i][j]==1){
-                    vis[i][j] = 1;
-                    q.push({i,j});
+            for(int j =0 ;j<n;j++){
+                if((i==0 || j == 0 || i==m-1 || j==n-1) && 
+                grid[i][j] == 1 && !vis[i][j]){
+                    dfs(i,j,grid);
                 }
             }
         }
-        
-        int delRow[] = {-1,0,1,0};
-        int delCol[] = {0,1,0,-1};
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            
-            q.pop();
-            
-            for(int i =0;i<4;i++){
-                int r = row + delRow[i];
-                int c = col + delCol[i];
-                
-                if(r>=0 and r<m and c>=0 and c<n and vis[r][c]==0 and grid[r][c]==1){
-                    vis[r][c] = 1;
-                    q.push({r,c});
-                }
-            }
-            
-        }
-        
-        // get all unvisited cells
-        
-        int count = 0;
+
+        int ans = 0;
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(vis[i][j]==0 and grid[i][j]==1)  count++;
+                if(grid[i][j]==1 && !vis[i][j]){
+                    ans += dfs(i,j,grid);
+                }
             }
         }
-        return count;
+
+        return ans;
     }
 };
