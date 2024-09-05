e335f6764bb99fe0246892a4316621e23a983059
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-09-05 15:05:43 +0530

    Time: 1078 ms (5.02%) | Memory: 43 MB (16.60%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,59 +1,57 @@
 class Solution {
-
 private:
     int m, n;
     vector<vector<bool>> vis;
 
-    bool ok(int x, int y){
-        return (x>=0 && x<m && y>=0 && y<n && !vis[x][y]);
+    bool isValid(int x, int y) {
+        return (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]);
     }
     
-    //directions
-    vector<pair<int,int>> dir{{0,-1},{-1,0},{0,1},{1,0}};
+    vector<pair<int, int>> directions{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     
-
-    // dfs with size
-    int dfs(int x, int y, vector<vector<int>> &grid){
+    int dfs(int x, int y, vector<vector<int>>& grid) {
         vis[x][y] = true;
-        int count = 1;
+        int count = 1; // Start counting with the current cell
 
-        for(auto [dx,dy]:dir){
+        for (auto [dx, dy] : directions) {
             int nx = x + dx;
             int ny = y + dy;
 
-            if(ok(nx,ny) && grid[nx][ny]==1){
-                count += dfs(nx,ny,grid);
+            if (isValid(nx, ny) && grid[nx][ny] == 1) {
+                count += dfs(nx, ny, grid);
             }
         }
 
         return count;
     }
+
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
 
-        vis.resize(m,vector<bool>(n,false));
+        vis.resize(m, vector<bool>(n, false));
 
-        // boundary marking
-        for(int i = 0;i<m;i++){
-            for(int j =0 ;j<n;j++){
-                if((i==0 || j == 0 || i==m-1 || j==n-1) && 
-                grid[i][j] == 1 && !vis[i][j]){
-                    dfs(i,j,grid);
+        // Mark boundary-connected land cells
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && 
+                    grid[i][j] == 1 && !vis[i][j]) {
+                    dfs(i, j, grid);
                 }
             }
         }
 
-        int ans = 0;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(grid[i][j]==1 && !vis[i][j]){
-                    ans += dfs(i,j,grid);
+        int enclaveCount = 0;
+        // Count land cells not connected to the boundary
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && !vis[i][j]) {
+                    enclaveCount += dfs(i, j, grid);
                 }
             }
         }
 
-        return ans;
+        return enclaveCount;
     }
 };
