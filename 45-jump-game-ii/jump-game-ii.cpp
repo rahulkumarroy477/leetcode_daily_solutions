8e9ad37cd75244628318fc976ac0c9f6c38c11c2
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-09-13 23:12:38 +0530

    Time: 12 ms (55.28%) | Memory: 19.3 MB (78.31%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
         int n = nums.size();
-        vector<int> dp(n,INT_MAX);
-        dp[0] = 0;
-        for(int i = 0;i<n-1;i++){
-            int curr = nums[i];
-            for(int k = i+1;k<n and k<=i+curr;k++){
-                dp[k] = min(dp[i]+1,dp[k]); 
+        int jumps = 0, l = 0, r = 0;
+
+        while(r<n-1){
+            int farthest = 0;
+
+            for(int i = l;i<=r;i++){
+                farthest = max(farthest, nums[i]+i);
             }
+            l = r+1;
+            jumps++;
+            r = farthest;
         }
-        return dp[n-1];
-        
+
+        return jumps;
     }
 };
