576b54016066c7cb14019f45800b3648d5bf170b
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-09-13 22:55:43 +0530

    Time: 45 ms (52.42%) | Memory: 50.9 MB (93.23%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int maxDist = 0;
         int n = nums.size();
-        if(n==1)    return true;
-        
-        // dp
-        
-//         vector<bool> dp(n,false);
-//         dp[0] = true;
-//         for(int i = 0;i<n;i++){
-//             if(dp[i]==false)    continue;
-//             for(int j = i+1;j<n and j<=nums[i]+i;j++){
-//                 dp[j] = dp[j] or true;
-//             }
-            
-//         }
-//         return dp[n-1];
-        
-        // greedy
-        
-        int max_dist = 0;
         for(int i = 0;i<n;i++){
-            if(max_dist<i)  return false;   // cannot reach to current index
-            
-            max_dist = max(max_dist,i+nums[i]);
-            if(max_dist>=n-1)   return true;
+            if(maxDist < i) return false;
+
+            maxDist = max(maxDist, i + nums[i]);
+            if(maxDist >= n-1)  return true;
         }
+
         return false;
     }
 };
