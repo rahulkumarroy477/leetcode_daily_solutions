feb3e6eb2ed4b794006be832175d9c1cac5a79db
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-10-30 21:34:11 +0530

    Time: 6 ms (41.36%) | Memory: 14.7 MB (81.98%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,63 +9,61 @@
  */
 class Solution {
 public:
-    
-    unordered_map<TreeNode*,TreeNode*> mp;
-    void childToPar(TreeNode* root){
-        if(!root)   return;
 
-        if(root->left){
-            mp[root->left] = root;
-            childToPar(root->left);
-        }
-        if(root->right){
-            mp[root->right] = root;
-            childToPar(root->right);
-        }
+    // child to parent mapping
+    unordered_map<TreeNode*, TreeNode*> mp;
+
+    void traversal(TreeNode* root){
+        if(root == NULL) return;
+
+        if(root->left) mp[root->left] = root;
+        traversal(root->left);
+        if(root->right) mp[root->right] = root;
+        traversal(root->right);
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        
-        if(!root)   return {};
-        childToPar(root);
+        traversal(root);
+        vector<int> ans;
+
         unordered_set<TreeNode*> vis;
-        int c = 0;
         queue<TreeNode*> q;
         q.push(target);
         vis.insert(target);
-
-        vector<int> ans;
+        int count = 0;
         while(!q.empty()){
             int size = q.size();
+            // If we've reached the desired distance k, collect nodes and stop further processing
+            if (count == k) {
+                for (int i = 0; i < size; ++i) {
+                    ans.push_back(q.front()->val);
+                    q.pop();
+                }
+                break;
+            }
 
             for(int i = 0;i<size;i++){
-                TreeNode* node = q.front();
+                auto node = q.front();
                 q.pop();
 
-                if(c == k)  ans.push_back(node->val);
-                // left
-                if(node->left && vis.find(node->left) == vis.end())
-                {
-                    vis.insert(node->left);
+                if(node->left && vis.find(node->left) == vis.end()){
                     q.push(node->left);
+                    vis.insert(node->left);
                 }
-                // right
-                if(node->right && vis.find(node->right) == vis.end())
-                {
-                    vis.insert(node->right);
+                if(node->right && vis.find(node->right) == vis.end()){
                     q.push(node->right);
+                    vis.insert(node->right);
                 }
-                // parent
-                if(mp[node] && vis.find(mp[node]) == vis.end()){
-                    vis.insert(mp[node]);
+
+                if(mp.find(node) != mp.end() and vis.find(mp[node]) == vis.end()){
                     q.push(mp[node]);
+                    vis.insert(mp[node]);
                 }
             }
-            c++;
 
-            if(c>k) break;
+            count++;
         }
 
         return ans;
-
     }
 };
+
