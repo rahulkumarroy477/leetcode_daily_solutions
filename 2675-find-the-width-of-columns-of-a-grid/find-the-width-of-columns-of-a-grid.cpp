a3e7e7555b7f5de4226e78ec98d2c3c33ae94c3e
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-11-03 23:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (85.16%) - LeetSync

--- 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.cpp
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.cpp
@@ -17,16 +17,16 @@ public:
         int row = grid.size();
         int col = grid[0].size();
 
-        vector<int> ans(col,0);
+        // vector<int> ans(col,0);
         for(int i = 0;i<col;i++){
             int curr = 0;
             for(int j = 0;j < row; j++){
                 curr = max(curr,countDigits(grid[j][i]));
             }
-            ans[i] = curr;
+            grid.back()[i] = curr;
         }
 
-        return ans;
+        return grid.back();
         
     }
 };
