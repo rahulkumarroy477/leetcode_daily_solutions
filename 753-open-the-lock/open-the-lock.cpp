ed20a12ea1daafb7f24c0c62f3a6991388671fe2
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-11-07 16:49:34 +0530

    Time: 118 ms (85.37%) | Memory: 41.2 MB (39.01%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-    void fillNeighbors(queue<string>& que, string& curr, unordered_set<string>& dead) {
-        for(int i = 0; i<4; i++) {
-            char ch  = curr[i];
-
-            char dec = ch=='0' ? '9' : ch-1;
-            char inc = ch=='9' ? '0' : ch+1;
-
-            curr[i]  = dec;
-            if(!dead.count(curr)) {
-                dead.insert(curr);
-                que.push(curr);
-            }
-
-            curr[i] = inc;
-            if(!dead.count(curr)) {
-                dead.insert(curr);
-                que.push(curr);
-            }
-            curr[i] = ch;
-        }
-    }
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string> dead(deadends.begin(),deadends.end());
-        
-        int level = 0;
-        
+        unordered_set<string> de(deadends.begin(),deadends.end());
+
         string start = "0000";
-        
-        if(dead.count(start) or dead.count(target))
-            return -1;
-        queue<string> q;
-        q.push(start);
+        if(de.find(start) != de.end())  return -1;
+
+        // unordered_set<string> vis;
+        queue<pair<string,int>> q;
+        q.push({start,0});
+        de.insert(start);
         while(!q.empty()){
-            int n = q.size();
+            auto [node,dist] = q.front();
+            q.pop();
+
+            if(node == target)  return dist;
             
-            while(n--){
-                string curr = q.front();
-                q.pop();
-                
-                if (curr == target) 
-                    return level;
-                
-                fillNeighbors(q,curr,dead);
-                
+            for(int i = 0;i<4;i++){
+                char ch = node[i];
+
+                // Move dial one step forward
+                node[i] = (ch == '9') ? '0' : ch + 1;
+                if (de.find(node) == de.end()) {
+                    de.insert(node);  // Mark as visited
+                    q.push({node, dist + 1});
+                }
+
+                // Move dial one step backward
+                node[i] = (ch == '0') ? '9' : ch - 1;
+                if (de.find(node) == de.end()) {
+                    de.insert(node);  // Mark as visited
+                    q.push({node, dist + 1});
+                }
+
+                // Restore the original state of node[i]
+                node[i] = ch;
             }
-            
-            level++;
-            
-            
+
         }
+
         return -1;
     }
 };
